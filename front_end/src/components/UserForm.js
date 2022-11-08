5205b9a8e1d82b9dc9062370557d87d9ecf3a75a
MarkizOrias <georgi.bargan@gmail.com>
2022-11-08 11:16:00 +0100

    modified:   front_end/src/App.css 	modified:   front_end/src/components/UserForm.js 	modified:   front_end/src/components/Wallet.js

--- front_end/src/components/UserForm.js
+++ front_end/src/components/UserForm.js
@@ -30,9 +30,9 @@ const UserForm = () => {
             <h2 className="form-check-msg">Store your copyrights on a blockchain in just 5 steps!</h2>
             <br />
             <form onSubmit={handleSubmit}>
-                <h3 className="form-check"><span className="badge bg-custom mar-bot"> 1 </span>&nbsp;Select the file from your system< br /><input className="form-control" type="file" id="type-input" name="typeInput1" /></h3>
-                <h3 className="form-check"><span className="badge bg-custom mar-bot"> 2 </span>&nbsp;Title of your work < br /><input className="form-control" type="text" id="title-input" name="typeInput2" /></h3>
-                <h3 className="form-check"><span className="badge bg-custom mar-bot"> 3 </span>&nbsp;Your name<br /><input className="form-control" type="text" id="name-input" name="typeInput3" /></h3>
+                <h3 className="form-check"><span className="badge bg-custom mar-bot"> 1 </span>&nbsp;Select the file from your system<span className="red">*</span>< br /><input className="form-control" type="file" id="type-input" name="typeInput1" /></h3>
+                <h3 className="form-check"><span className="badge bg-custom mar-bot"> 2 </span>&nbsp;Title of your work<span className="red">*</span>< br /><input className="form-control" type="text" id="title-input" name="typeInput2" /></h3>
+                <h3 className="form-check"><span className="badge bg-custom mar-bot"> 3 </span>&nbsp;Your name<span className="red">*</span><br /><input className="form-control" type="text" id="name-input" name="typeInput3" /></h3>
                 <h3 className="form-check"><span className="badge bg-custom mar-bot"> 4 </span>&nbsp;Co-author<br /><input className="form-control" type="text" id="co-author-input" name="typeInput4" /></h3>
                 <h3 className="form-check"><span className="badge bg-custom mar-bot"> 5 </span>&nbsp;Store your copyrights on-chain<br /><button className="button" id="store-button" name="store-button">Store</button></h3>
             </form>
